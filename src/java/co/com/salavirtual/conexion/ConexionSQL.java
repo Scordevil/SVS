6df4bb66554e11d6cb8cf872776de1bc63b64880
Administrator <Administrator@WIN-OLQLLN0KJR5>
2016-10-03 13:00:36 +0000

    Web servise graficas reportes

--- src/java/co/com/salavirtual/conexion/ConexionSQL.java
+++ src/java/co/com/salavirtual/conexion/ConexionSQL.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
 public class ConexionSQL {
 
   //  static String clave = "yU7eywfXILoZjtaD";
-    static String clave = "12345678";
+    static String clave = "";
     static Statement ST = null;
     static Connection cn = null;
 
